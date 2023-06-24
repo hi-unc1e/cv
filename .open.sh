ace0d943dc86d90e7724b14e4874c014b7ea64a4
unc1e <root@zuoxueba.org>
2023-06-24 23:00:34 +0800

    workflow

---
+++ .open.sh
@@ -0,0 +1,8 @@
+# open vs.code
+code /Users/dpdu/PycharmProjects/cv
+
+# open Typora
+open -a Typora /Users/dpdu/PycharmProjects/cv/README.md
+
+# open EDGE
+open -a "Microsoft Edge" https://github.com/hi-unc1e/cv/
